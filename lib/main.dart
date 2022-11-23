50afa5cd927a661c148ffbb79705752e044755fe
trd-wilfredo <trd.wilfredomadelo@gmail.com>
2022-11-23 11:41:21 +0800

    lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:worl_time/pages/home.dart';
+import 'package:quote_app/pages/home.dart';
+
 void main() {
-  runApp(const MaterialApp(
-    home:Home()
-  ));
+  runApp(const MaterialApp(home: Home()));
 }
