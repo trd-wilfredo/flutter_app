857583c5f44b292fd2021b20d322650da6c52904
trd-wilfredo <trd.wilfredomadelo@gmail.com>
2022-11-23 11:39:23 +0800

    lib

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+        'Choose Location Page'
+      ),
+    );
+  }
+}
