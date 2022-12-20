211552b8ee38544296d9fc3b69d9c5eeb56db358
trd-wilfredo <trd.wilfredomadelo@gmail.com>
2022-12-20 10:54:48 +0800

    fix_auth

---
+++ lib/pages/login/service/auth_service.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_features/pages/login/service/database_service.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // login
+
+  Future loginInWithEmailandPassword(String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      return e.message;
+    }
+  }
+
+  // register
+
+  Future registerUserWithEmailandPassword(
+      String fullname, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        // database call
+        await DatabaseService(uid: user.uid).updateUserData(fullname, email);
+
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      return e.message;
+    }
+  }
+
+  // Signout
+}
