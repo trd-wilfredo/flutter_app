8b90f65cb0ef771f68e65ac591ceec94376c4b05
trd-wilfredo <trd.wilfredomadelo@gmail.com>
2023-02-28 16:05:45 +0800

    progress bar

--- lib/pages/page.dart
+++ lib/pages/page.dart
@@ -2,6 +2,14 @@ class PageList {
   String page;
   String part;
   String title;
+  double barPecent;
+  String percent;
 
-  PageList({required this.page, required this.part, required this.title});
+  PageList({
+    required this.page,
+    required this.part,
+    required this.title,
+    required this.barPecent,
+    required this.percent,
+  });
 }
--- lib/pages/page_card.dart
+++ lib/pages/page_card.dart
@@ -9,36 +9,62 @@ class PageListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(color: Colors.grey),
-          left: BorderSide(color: Colors.grey),
-          bottom: pageprops.part == 'bottom'
-              ? BorderSide(color: Color.fromRGBO(0, 0, 0, 1), width: 3.0)
-              : BorderSide(color: Color.fromARGB(0, 255, 255, 255)),
-          right: BorderSide(color: Colors.grey),
+    return Row(
+      children: [
+        Container(
+          width: 400,
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.grey),
+              left: BorderSide(color: Colors.grey),
+              bottom: pageprops.part == 'bottom'
+                  ? BorderSide(color: Color.fromRGBO(0, 0, 0, 1), width: 3.0)
+                  : BorderSide(color: Color.fromARGB(0, 255, 255, 255)),
+              right: BorderSide(color: Colors.grey),
+            ),
+          ),
+          child: TextButton(
+            style: TextButton.styleFrom(
+              alignment: Alignment.centerLeft,
+              primary: Colors.black, // foreground
+            ),
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/' + pageprops.page,
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 0.0, bottom: 0.0),
+              child: Text(
+                pageprops.title,
+              ),
+            ),
+          ),
+          height: 40.0,
         ),
-      ),
-      child: TextButton(
-        style: TextButton.styleFrom(
-          alignment: Alignment.centerLeft,
-          primary: Colors.black, // foreground
+        Container(
+          width: 15,
         ),
-        onPressed: () {
-          Navigator.pushNamed(
-            context,
-            '/' + pageprops.page,
-          );
-        },
-        child: Padding(
-          padding: const EdgeInsets.only(left: 20.0, top: 0.0, bottom: 0.0),
-          child: Text(
-            pageprops.title,
+        Container(
+          width: 145,
+          height: 30,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Color.fromARGB(255, 10, 0, 0),
+            ),
+          ),
+          child: LinearProgressIndicator(
+            value: pageprops.barPecent, // Defaults to 0.5.
+            color: Color.fromARGB(255, 0, 0, 0),
+            backgroundColor: Colors.white,
           ),
         ),
-      ),
-      height: 40.0,
+        Container(
+          width: 5,
+        ),
+        Text(pageprops.percent)
+      ],
     );
   }
 }
